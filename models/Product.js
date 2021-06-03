5e7c60bc243573b6c57534e03825481c6df046f8
danilbek <deni200040@gmail.com>
2021-06-04 00:16:23 +0300

    исправил модель

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Product = new mongoose.Schema({
   nameProduct: { type: String, required: true },
-  brand: { type: mongoose.Types.ObjectId, required: true },
+  brand: { type: mongoose.Types.ObjectId, required: true, ref: "brand" },
   category: { type: mongoose.Types.ObjectId, required: true, ref: "category" },
   price: { type: String, required: true },
   size: { type: String, required: true },
