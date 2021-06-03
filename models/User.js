97644d149c3b5e312dd05024f86c697e84fded5f
danilbek <deni200040@gmail.com>
2021-06-03 22:11:27 +0300

    теперь все

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.Schema({
+  userName: { type: String, requierd: true },
+  email: { type: String, require: true },
+  login: { type: String, required: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+})
+
+module.exports = User;
