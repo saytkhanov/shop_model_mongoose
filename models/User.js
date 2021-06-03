08aacc6d7626a159cf68ac1c5f5315cde713ff87
danilbek <deni200040@gmail.com>
2021-06-03 22:52:40 +0300

    теперь точно  все

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const User = mongoose.Schema({
+const User = new mongoose.Schema({
   userName: { type: String, requierd: true },
   email: { type: String, require: true },
   login: { type: String, required: true },
