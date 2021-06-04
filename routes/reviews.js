bd6427459be3fbc75dbd48bbb28677fd718561c2
Abu <abubakr.ingush@gmail.com>
2021-06-04 10:38:25 +0300

    Добавил роут для отзывов

---
+++ routes/reviews.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+const controllers = require('../controllers/reviews')
+const router = Router();
+
+router.get('/products/:id/reviews', controllers.getProductReviews)
+router.post('/review', controllers.postReview)
+router.delete('/delete/review/:id', controllers.deleteReview)
