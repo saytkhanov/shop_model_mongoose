03fd7e7ac7f71602b1c655d32d49268237686688
saytkhanov <saythanov2014@mail.ru>
2021-06-04 11:09:01 +0300

    a

--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-const router = Router();
+const router = Router()
 const { getCategories, postCategories, patchCategories, deleteCategories } = require('../controllers/categories');
 
-router.use("/categories", getCategories)
-router.use("/categories", postCategories)
-router.use("/categories/:id", patchCategories)
-router.use("/categories/:id", deleteCategories)
+router.get("/categories", getCategories)
+router.post("/categories", postCategories)
+router.patch("/categories/:id", patchCategories)
+router.delete("/categories/:id", deleteCategories)
 
-module.exports = router;
+module.exports = router
