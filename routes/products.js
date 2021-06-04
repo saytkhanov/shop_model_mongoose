59db64887dc069c7b2ebf3eb555c46f19ac0531a
danilbek <deni200040@gmail.com>
2021-06-04 15:24:54 +0300

    finish

--- routes/products.js
+++ routes/products.js
@@ -4,7 +4,8 @@ const productController = require("../controllers/products");
 const router = Router();
 
 router.get("/products", productController.get);
-router.get("/products/:id/categories", productController.getById);
+router.get("/products/:id/categories", productController.getByCategory);
+router.get("/products/:id/brand", productController.getByBrand);
 router.post("products/:id", productController.create);
 router.patch("products/:id", productController.update);
 router.delete("products/:id", productController.delete);
